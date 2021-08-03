839e00d016060ab4d03ebc4dbcc0bc3d38486c34
shikhars371 <shikhars371@gmail.com>
2021-08-03 13:15:58 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [value, setCount] = React.useState(0);
+    const [value2, setCount2] = React.useState(0);
+
+    return (
+
+        <div className="main">
+            <textarea rows="3" style={{margin: "15px"}} onChange={e => setCount(e.target.value)} ></textarea>
+            <p>{value}</p>
+            <p>{value.length}</p>
+
+            <textarea rows="3" style={{margin: "15px"}} onChange={e => setCount2(e.target.value)} ></textarea>
+            <p>{value2}</p>
+            <p>{value2.length}</p>
+
+            {((value === value2) ? <p>Same</p> : <p>Not same</p>)}
+
+        </div>
+
+    );
 }
 
 export default App;
